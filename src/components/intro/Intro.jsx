8890972a228425580bba1b59984e05665a42e5d1
JamesChaves <james.chaves@sinco.com.co>
2023-08-26 05:26:58 -0500

    text animations added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,17 @@ export default function Intro() {
   return (
 
     <section className='banner'>
-      <h2>Find & Add </h2>
+      <h2>Find <b>&</b> Add </h2>
+    <div class="wrapper">
+      <div class="letter">C</div>
+      <div class="letter">O</div>
+      <div class="letter">L</div>
+      <div class="letter">L</div>
+      <div class="letter">E</div>
+      <div class="letter">C</div>
+      <div class="letter">T</div>
+    </div>
+    <div className="textShadow"></div>
       <span>
         Your Favorite Characters at a push of a key
       </span>
